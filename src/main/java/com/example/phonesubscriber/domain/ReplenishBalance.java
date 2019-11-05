b21a775363889a9fe676df4cec07e5246a0677e8
Nihrom77 <snoll@yandex.ru>
2019-11-05 18:34:13 +0300

    Добавил форму пополнения баланса. Добавил на страницы ссылки на возвращения на главную.

---
+++ src/main/java/com/example/phonesubscriber/domain/ReplenishBalance.java
@@ -0,0 +1,24 @@
+package com.example.phonesubscriber.domain;
+
+public class ReplenishBalance {
+
+    private int amount;
+   private String msisdn;
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getMsisdn() {
+        return msisdn;
+    }
+
+    public void setMsisdn(String msisdn) {
+        this.msisdn = msisdn;
+    }
+
+}
--- src/main/java/com/example/phonesubscriber/rest/SubscriberController.java
+++ src/main/java/com/example/phonesubscriber/rest/SubscriberController.java
@@ -1,6 +1,7 @@
 package com.example.phonesubscriber.rest;
 
 import com.example.phonesubscriber.domain.Prices;
+import com.example.phonesubscriber.domain.ReplenishBalance;
 import com.example.phonesubscriber.domain.Subscriber;
 import com.example.phonesubscriber.repository.PricesRepository;
 import com.example.phonesubscriber.repository.SubscriberRepository;
@@ -8,6 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -34,21 +39,78 @@ public class SubscriberController {
 
     @GetMapping("/makecall")
     public String makecallPage(@RequestParam("msisdn") String msisdn, Model model) {
-        model.addAttribute("msisdn",msisdn);
+        model.addAttribute("msisdn", msisdn);
         Prices price = pricesRepo.findAll().get(0);
-        model.addAttribute("isPresent", false);
+        model.addAttribute("result", "Абонент не найден");
         subsRepo.findAll().stream().filter((Subscriber s) -> s.getMsisdn().equals(msisdn)).findAny().ifPresent(
                 (Subscriber s) -> {
                     int b = s.getBalance();
                     if (b - price.getCallPrice() >= 0) {
                         s.setBalance(b - price.getCallPrice());
                         s.setStatus(s.getBalance() > 0 ? s.ACTIVE : s.BLOCKED);
+                        model.addAttribute("result", "Звонок выполнен");
+                    } else {
+                        model.addAttribute("result", "Не достаточно средств для звонка.");
                     }
-                    model.addAttribute("isPresent", true);
-                    model.addAttribute("subscriber", s);
+
+                    subsRepo.save(s);
                 }
         );
+
         return "makecall";
+    }
+
+    @GetMapping("/sendsms")
+    public String sendSMSPage(@RequestParam("msisdn") String msisdn, Model model) {
+        model.addAttribute("msisdn", msisdn);
+        Prices price = pricesRepo.findAll().get(0);
+        model.addAttribute("result", "Абонент не найден");
+        subsRepo.findAll().stream().filter((Subscriber s) -> s.getMsisdn().equals(msisdn)).findAny().ifPresent(
+                (Subscriber s) -> {
+                    int b = s.getBalance();
+                    if (b - price.getSmsPrice() >= 0) {
+                        s.setBalance(b - price.getSmsPrice());
+                        s.setStatus(s.getBalance() > 0 ? s.ACTIVE : s.BLOCKED);
+                        model.addAttribute("result", "SMS отправлена");
+                    } else {
+                        model.addAttribute("result", "Не достаточно средств для отправки SMS.");
+                    }
+
+                    subsRepo.save(s);
+                }
+        );
+
+        return "sendsms";
+    }
+
+    @GetMapping("/replenishBalancePage")
+    public String replenishBalancePage(@RequestParam("msisdn") String msisdn, Model model) {
+        model.addAttribute("msisdn", msisdn);
+        subsRepo.findAll().stream().filter((Subscriber s) -> s.getMsisdn().equals(msisdn)).findAny().ifPresent(
+                (Subscriber s) -> {
+                    ReplenishBalance b = new ReplenishBalance();
+                    b.setMsisdn(msisdn);
+                    model.addAttribute("repleinesh", b);
+                }
+        );
+
+        return "replenishBalancePage";
+    }
+
+    @PostMapping("/replenishBalancePage")
+    public String replenishBalanceSubmit(@ModelAttribute ReplenishBalance balance, Model model) {
+        model.addAttribute("result", "Абонент не найден");
+        model.addAttribute("msisdn", balance.getMsisdn());
+        subsRepo.findAll().stream().filter((Subscriber s) -> s.getMsisdn().equals(balance.getMsisdn())).findAny().ifPresent(
+                (Subscriber s) -> {
+                    if (balance.getAmount() > 0) {
+                        s.setBalance(s.getBalance() + balance.getAmount());
+                    }
+                    subsRepo.save(s);
+                    model.addAttribute("result", "Баланс успешно пополнен. Текущий баланс = " + s.getBalance());
+                }
+        );
+        return "replenishBalanceResult";
     }
 
 }
